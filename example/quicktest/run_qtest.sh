cc58c4756d298e64b722cca813860057ef9b8792
Qianqian Fang <fangq@nmr.mgh.harvard.edu>
2010-02-25 15:18:55 +0000

    ati example worked
    git-svn-id: https://orbit.nmr.mgh.harvard.edu/svn/mcxcl/mcxcl@13 4848f366-3658-0410-a320-90152d9d5b07

--- example/quicktest/run_qtest.sh
+++ example/quicktest/run_qtest.sh
@@ -4,6 +4,7 @@ if [ ! -e semi60x60x60.bin ]; then
   perl -pi -e 's/\x0/\x1/g' semi60x60x60.bin
 fi
 
-#time ../../bin/mcx -t 1024 -g 10 -m 795590 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -p 18
-#time ../../bin/mcxcl -t 8192 -T 128 -g 10 -m 100000 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -k ../../src/mcx_core.cl
-time ../../bin/mcxcl -t 1024 -T 128 -g 10 -m 100000 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -k ../../src/mcx_core.cl
+# use -T 256 gives huge speed boost for ATI cards
+time ../../bin/mcxcl -t 2048 -T 256 -g 10 -m 1000000 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -k ../../src/mcx_core.cl
+# use CPU backend, set CPU_MAX_COMPUTE_UNITS=n to specify number of CPU cores
+#time ../../bin/mcxcl -t 2048 -T 256 -g 10 -m 100000 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -k ../../src/mcx_core.cl -c
