e5f2881665c130f627eece32bbbeafc09ea9ff33
Qianqian Fang <fangqq@gmail.com>
2020-04-02 22:45:04 +0100

    migrate windows compilation to msys2

---
+++ src/buildmcxcl.m
@@ -0,0 +1,95 @@
+function buildmmc(varargin)
+%
+% Format:
+%    buildmex or buildmex('option1',value1,'option2',value2,...)
+%
+% Compiling script for mmclab mex file in MATLAB and GNU Octave. 
+% If compiled successfully, the output mex file can be found in the 
+% mmc/mmclab folder (or ../mmclab using relative path from mmc/src)
+%
+% Author: Qianqian Fang <q.fang at neu.edu>
+%
+% Input:
+%    options: without any option, this script compiles mmc.mex* using
+%    default settings. Supported options include
+%      'include': a string made of sequences of ' -I"/folder/path" ' that 
+%            can be included for compilation (format similar to the -I
+%            option for gcc)
+%      'lib': a string made of sequences of ' -L"/folder/path" ' and '
+%           -llibrary' that can be added for linking (format similar to -L
+%           and -l flags for gcc)
+%      'filelist': a user-defined list of source file names
+%
+% Dependency (Windows only):
+%  1.To compile mmclab in MATLAB R2017a or earlier on Windows, you must 
+%    pre-install the MATLAB support for MinGW-w64 compiler 
+%    https://www.mathworks.com/matlabcentral/fileexchange/52848-matlab-support-for-mingw-w64-c-c-compiler
+%  2.After installation of MATLAB MinGW support, you must type "mex -setup"
+%    in MATLAB and select "MinGW64 Compiler (C)". 
+%  3.Once you select the MingW C compiler, you should run "mex -setup"
+%    again in MATLAB and select "MinGW64 Compiler (C++)" to compile C++.
+%  4.File C:\Windows\System32\OpenCL.dll must exist. You can obtain this
+%    file by installing your graphics driver or install CUDA/AMD GPU SDK
+%    and copy OpenCL.dll to the C:\Windows\System32 folder.
+%
+% This function is part of Mesh-based Monte Carlo (MMC) URL: http://mcx.space
+%
+% License: GNU General Public License version 3, please read LICENSE.txt for details
+%
+
+cd(fileparts(which(mfilename)));
+if(nargin==1 && strcmp(varargin{1},'clean'))
+    if(~isempty(dir('*.o')))
+        delete('*.o'); 
+    end
+    return;
+end
+opt=struct(varargin{:});
+pname='mcx';
+
+cflags=' -g -pedantic -Wall -O3 -DMCX_EMBED_CL -DMCX_OPENCL -DUSE_OS_TIMER  -std=c99  -DMCX_CONTAINER -c ';
+
+filelist={'mcx_utils.c','tictoc.c','cjson/cJSON.c','mcx_host.cpp',...
+    'mcxcl.c','mcx_shapes.c','mcxlabcl.cpp'};
+if(isfield(opt,'filelist'))
+    filelist=opt.filelist;
+end
+if(isfield(opt,'include'))
+    cflags=[cflags ' ' opt.include];
+end
+if(ispc)
+    linkflags='$LINKLIBS -fopenmp -lstdc++ -static';
+    cflags=[cflags ' -I./mingw64/include -I"$MINGWROOT/opt/include"'];
+    linkflags=[linkflags ' ''C:\Windows\System32\OpenCL.dll'' '];
+    linkvar='LINKLIBS';
+else
+    linkflags='\$CLIBS -fopenmp -static-libgcc -static-libstdc++';
+    cflags=[cflags ' -fPIC '];
+    linkflags=[linkflags ' -lOpenCL '];
+    linkvar='CLIBS';
+end
+if(~exist('OCTAVE_VERSION','builtin'))
+    for i=1:length(filelist)
+        flag='CFLAGS';
+        if(regexp(filelist{i},'\.[Cc][Pp][Pp]$'))
+            flag='CXXFLAGS';
+        end
+        disp(sprintf('mex OBJEXT=.o %s=''%s'' -c ''%s'' ',flag,cflags,filelist{i}));
+        eval(sprintf('mex OBJEXT=.o %s=''%s'' -c ''%s'' ',flag,cflags,filelist{i}));
+    end
+    if(isfield(opt,'lib'))
+        linkflags=[linkflags ' ' opt.lib];
+    end
+    fn=dir('*.o');
+    fprintf('mex %s -output %scl -outdir ../%slabcl %s=''%s'' \n',strjoin({fn.name}),pname,pname,linkvar,linkflags);
+    eval(sprintf('mex %s -output %scl -outdir ../%slabcl %s=''%s'' ',strjoin({fn.name}),pname,pname,linkvar,linkflags));
+else
+    linkflags=regexprep(linkflags,['[\\]*\$' linkvar],'');
+    for i=1:length(filelist)
+        cmd=sprintf('mex %s -c ''%s'' ',cflags,filelist{i});
+        disp(cmd);
+        eval(cmd);
+    end
+    fn=dir('*.o');
+    eval(sprintf('mex %s -o ../%slabcl/%scl %s ',strjoin({fn.name}),pname,pname,linkflags));
+end
