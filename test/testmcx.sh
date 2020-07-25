3f413504c8218a2358f183a5d2dd990307d71810
Qianqian Fang <fangqq@gmail.com>
2020-07-25 15:24:57 -0400

    testmcx.sh accepts additional param

--- test/testmcx.sh
+++ test/testmcx.sh
@@ -3,6 +3,7 @@
 fail=0
 echo "test binary file ... "
 MCX=../bin/mcxcl
+PARAM=$@
 if [ ! -f $MCX ]; then echo "mcx binary does not exit"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test libraries ... "
@@ -41,43 +42,35 @@ temp=`$MCX --bench colin27 --dumpjson - --zip gzip | grep -o -E '"_ArrayZipData_
 if [ -z "$temp" ]; then echo "fail to set gzip compression for volume exporting"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test homogeneous domain simulation ... "
-temp=`$MCX --bench cube60 | grep -o -E 'absorbed:.*17\.[0-9]+%'`
+temp=`$MCX --bench cube60 $PARAM | grep -o -E 'absorbed:.*17\.[0-9]+%'`
 if [ -z "$temp" ]; then echo "fail to run cube60 benchmark"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test boundary reflection ... "
-temp=`$MCX --bench cube60b | grep -o -E 'absorbed:.*27\.[0-9]+%'`
+temp=`$MCX --bench cube60b $PARAM | grep -o -E 'absorbed:.*27\.[0-9]+%'`
 if [ -z "$temp" ]; then echo "fail to run cube60b benchmark"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test boundary reflection flag -b ... "
-temp=`$MCX --bench cube60 -b 1 | grep -o -E 'absorbed:.*27\.[0-9]+%'`
+temp=`$MCX --bench cube60 -b 1 $PARAM | grep -o -E 'absorbed:.*27\.[0-9]+%'`
 if [ -z "$temp" ]; then echo "fail to use -b 1 flag to enable reflection"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test boundary condition flag -B ... "
-temp=`$MCX --bench cube60 -b 0 -B aarraa | grep -o -E 'absorbed:.*27\.[0-9]+%'`
+temp=`$MCX --bench cube60 -b 0 -B aarraa $PARAM | grep -o -E 'absorbed:.*27\.[0-9]+%'`
 if [ -z "$temp" ]; then echo "fail to use -B flag to set facet based boundary condition"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test photon detection ... "
-temp=`$MCX --bench cube60b | grep -o -E 'detected.*4[0-9]{3,3} photons'`
+temp=`$MCX --bench cube60b $PARAM | grep -o -E 'detected.*4[0-9]+ photons'`
 if [ -z "$temp" ]; then echo "fail to detect photons in the cube60b benchmark"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test planary widefield source ... "
-temp=`$MCX --bench cube60planar | grep -o -E 'absorbed:.*25\.[0-9]+%'`
+temp=`$MCX --bench cube60planar $PARAM | grep -o -E 'absorbed:.*25\.[0-9]+%'`
 if [ -z "$temp" ]; then echo "fail to run cube60planar benchmark"; fail=$((fail+1)); else echo "ok"; fi
 
 echo "test heterogeneous domain ... "
-temp=`$MCX --bench spherebox | grep -o -E 'absorbed:.*1[01]\.[0-9]+%'`
+temp=`$MCX --bench spherebox $PARAM | grep -o -E 'absorbed:.*1[01]\.[0-9]+%'`
 if [ -z "$temp" ]; then echo "fail to run spherebox benchmark"; fail=$((fail+1)); else echo "ok"; fi
 
-echo "test default thread/block setting ... "
-temp=`$MCX --bench spherebox -A 0 | grep -o -E 'threadph=61 extra=576'`
-if [ -z "$temp" ]; then echo "fail to use default thread block settings in spherebox benchmark"; fail=$((fail+1)); else echo "ok"; fi
-
-echo "test user-defined thread/block setting ... "
-temp=`$MCX --bench spherebox -A 0 -t 20000 -T 128 | grep -o -E 'threadph=50 extra=1600 np=1000000 nthread=19968'`
-if [ -z "$temp" ]; then echo "fail to use default thread block settings in spherebox benchmark"; fail=$((fail+1)); else echo "ok"; fi
-
 echo "test progress bar ... "
-temp=`$MCX --bench cube60 -D P | grep 'Progress: .* 100%'`
+temp=`$MCX --bench cube60 -D P  $PARAM | grep 'Progress: .* 100%'`
 if [ -z "$temp" ]; then echo "fail to print progress bar"; fail=$((fail+1)); else echo "ok"; fi
 
 
