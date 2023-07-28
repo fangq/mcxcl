de38e429036435fc7706616d5855a93045982d75
Qianqian Fang <fangqq@gmail.com>
2023-07-28 17:35:26 -0400

    try mingw makefiles generator

--- pmcxcl/setup.py
+++ pmcxcl/setup.py
@@ -54,8 +54,9 @@ class CMakeBuild(build_ext):
         # Adding CMake arguments set as environment variable
         # (needed e.g. to build for ARM OSx on conda-forge)
         if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split('\t') if item]
 
+        print(self.compiler.compiler_type)
         if self.compiler.compiler_type != "msvc":
             # Using Ninja-build since it a) is available as a wheel and b)
             # multithreads automatically. MSVC would require all variables be
