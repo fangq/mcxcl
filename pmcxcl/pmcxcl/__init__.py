507b37b2a9fe965e91beb7102700bd95178eab7d
Qianqian Fang <fangqq@gmail.com>
2023-09-26 22:37:13 -0400

    fix continuous media scaling issue,pmcxcl 0.1.2, fangq/mcx#185

--- pmcxcl/pmcxcl/__init__.py
+++ pmcxcl/pmcxcl/__init__.py
@@ -53,7 +53,7 @@ except ImportError:  # pragma: no cover
 
 from .bench import bench
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = (
     "gpuinfo",
--- pmcxcl/setup.py
+++ pmcxcl/setup.py
@@ -119,7 +119,7 @@ class CMakeBuild(build_ext):
 setup(
     name="pmcxcl",
     packages=['pmcxcl'],
-    version="0.1.1",
+    version="0.1.2",
     requires=['numpy'],
     license='GPLv3+',
     author="Matin Raayai Ardakani, Qianqian Fang",
