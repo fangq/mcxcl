fangq/mcxcl

commit 9b4d76f03d86c2c9f3f449e340925649d4514379

fix boundary condition handling, port from mcx

src/mcx_core.cl
@@ -792,7 +792,7 @@ int launchnewphoton(float4* p, float4* v, float4* f, short4* flipdir, FLOAT4VEC*
      */
     if (GPU_PARAM(gcfg, savedet) == FILL_MAXDETPHOTON) {
         if (*dpnum >= GPU_PARAM(gcfg, maxdetphoton)) {
-            gprogress[0] = (GPU_PARAM(gcfg, threadphoton) >> 1) * 4.5f;
+            gprogress[0] = (gcfg->threadphoton >> 1) * 4.5f;
             return 1;
         }
     }
@@ -1485,7 +1485,8 @@ __kernel void mcx_main_loop(__global const uint* media,
         }
 
         /** launch new photon when exceed time window or moving from non-zero voxel to zero voxel without reflection */
-        if ((mediaid == 0 && (((isdet & 0xF) == 0 && (!GPU_PARAM(gcfg, doreflect) || (GPU_PARAM(gcfg, doreflect) && n1 == gproperty[0].w))) || (isdet == bcAbsorb || isdet == bcCyclic) )) || f.y > gcfg->twin1) {
+        if ((mediaid == 0 && ((!GPU_PARAM(gcfg, doreflect) || (GPU_PARAM(gcfg, doreflect) && n1 == gproperty[0].w)) || (((isdet & 0xF) == bcUnknown && !GPU_PARAM(gcfg, doreflect))
+                              || (isdet & 0xF) == bcAbsorb || (isdet & 0xF) == bcCyclic)) && (isdet & 0xF) != bcMirror && (isdet & 0xF) != bcReflect) ||  f.y > gcfg->twin1) {
             if (isdet == bcCyclic) {
                 if (flipdir.w == 0) {
                     p.x = mcx_nextafterf(convert_float_rte(((idx1d == OUTSIDE_VOLUME_MIN) ? gcfg->maxidx.x : 0)), (v.x > 0.f) - (v.x < 0.f));
@@ -1530,8 +1531,16 @@ __kernel void mcx_main_loop(__global const uint* media,
 
 #ifdef MCX_DO_REFLECTION
 
+        if (gcfg->mediaformat < 100) {
+            updateproperty(&prop, mediaid, gproperty, gcfg); ///< optical property across the interface
+        }
+
         //if hit the boundary, exceed the max time window or exit the domain, rebound or launch a new one
-        if (((GPU_PARAM(gcfg, doreflect) && (isdet & 0xF) == 0) || (isdet & 0x1)) && ((isdet & 0xF) == bcMirror || n1 != gproperty[(mediaid > 0 && GPU_PARAM(gcfg, mediaformat) >= 100) ? 1 : mediaid].w)) {
+        if (((mediaid && GPU_PARAM(gcfg, doreflect)) // if at an internal boundary, check cfg.isreflect flag
+                || (mediaid == 0 &&  // or if out of bbx or enters 0-voxel
+                    (((isdet & 0xF) == bcUnknown && GPU_PARAM(gcfg, doreflect)) // if cfg.bc is "_", check cfg.isreflect
+                     || (((isdet & 0xF) == bcReflect || (isdet & 0xF) == bcMirror)))))  // or if cfg.bc is 'r' or 'm'
+                && (((isdet & 0xF) == bcMirror) || n1 != ((GPU_PARAM(gcfg, mediaformat) < 100) ? (prop.w) : (gproperty[(mediaid > 0 && GPU_PARAM(gcfg, mediaformat) >= 100) ? 1 : mediaid].w)))) {
             float Rtotal = 1.f;
             float cphi, sphi, stheta, ctheta;
 
@@ -1554,7 +1563,10 @@ __kernel void mcx_main_loop(__global const uint* media,
                 GPUDEBUG(((__constant char*)"Rtotal=%f\n", Rtotal));
             }
 
-            if (Rtotal < 1.f && (((isdet & 0xF) == 0 && gproperty[mediaid].w >= 1.f) || isdet == bcReflect) &&  (isdet != bcMirror) && rand_next_reflect(t) > Rtotal) { // do transmission
+            if (Rtotal < 1.f // if total internal reflection does not happen
+                    && (!(mediaid == 0 && ((isdet & 0xF) == bcMirror))) // if out of bbx and cfg.bc is not 'm'
+                    && rand_next_reflect(t) > Rtotal) { // and if photon chooses the transmission path, then do transmission
+
                 transmit(&v, n1, prop.w, flipdir.w);
 
                 if (mediaid == 0) { // transmission to external boundary
@@ -1589,6 +1601,11 @@ __kernel void mcx_main_loop(__global const uint* media,
                 updateproperty(&prop, mediaid, gproperty, gcfg); ///< optical property across the interface
                 n1 = prop.w;
             }
+        }
+
+        if (mediaid == 0 || idx1d == OUTSIDE_VOLUME_MIN || idx1d == OUTSIDE_VOLUME_MAX) {
+            printf("ERROR: should never happen! mediaid=%d idx1d=%X gcfg->doreflect=%d n1=%f n2=%f isdet=%d flipdir[3]=%d p=(%f %f %f)[%d %d %d]\n", mediaid, idx1d, GPU_PARAM(gcfg, doreflect), n1, prop.w, isdet, flipdir.w, p.x, p.y, p.z, flipdir.x, flipdir.y, flipdir.z);
+            return;
         }
 
 #endif

mcxlabcl/examples/demo_colin27_atlas.m
@@ -56,7 +56,7 @@ cfg.seed=29012392;
 cfg.nphoton=10000000;
 cfg.issaveexit=1;
 
-[flue,detps]=mcxlab(cfg);
+[flue,detps]=mcxlabcl(cfg);
 mcxplotvol(log10(flue.data));
 figure;
 plotmesh(detps.p,'r.')

mcxlabcl/examples/demo_focus_mirror_bc.m
@@ -28,6 +28,6 @@ cfg.tend=5e-9;
 cfg.tstep=5e-09;
 cfg.bc='mm____';
 
-flux=mcxlab(cfg);
+flux=mcxlabcl(cfg);
 
 mcxplotvol(log10(double(flux.data)))

mcxlabcl/examples/demo_mcxlab_basic.m
@@ -1,34 +1,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% MCXLAB - Monte Carlo eXtreme for MATLAB/Octave by Qianqina Fang
-%
-% In this example, we show the most basic usage of MCXLAB.
-%
-% This file is part of Monte Carlo eXtreme (MCX) URL:http://mcx.sf.net
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-clear cfg cfgs
-cfg.nphoton=1e7;
-cfg.vol=uint8(ones(60,60,60));
-cfg.srcpos=[30 30 1];
-cfg.srcdir=[0 0 1];
-cfg.gpuid=1;
-% cfg.gpuid='11'; % use two GPUs together
-cfg.autopilot=1;
-cfg.prop=[0 0 1 1;0.005 1 0 1.37];
-cfg.tstart=0;
-cfg.tend=5e-9;
-cfg.tstep=1e-10;
-
-% calculate the flux distribution with the given config
-flux=mcxlabcl(cfg);
-
-
-cfgs(1)=cfg;
-cfgs(2)=cfg;
-cfgs(1).isreflect=0;
-cfgs(2).isreflect=1;
-cfgs(2).detpos=[30 20 1 1;30 40 1 1;20 30 1 1;40 30 1 1];
-% calculate the flux and partial path lengths for the two configurations
-[fluxs,detps]=mcxlabcl(cfgs);
-
-imagesc(squeeze(log(fluxs(1).data(:,30,:,1)))-squeeze(log(fluxs(2).data(:,30,:,1))));

mcxlabcl/examples/demo_mcxlab_replay.m
@@ -22,12 +22,12 @@ cfg.tstep=5e-10;
 % calculate the flux distribution with the given config
 cfg.detpos=[15 30 0 2];
 %cfg.savedetflag='dsp';
-[flux, detp, vol, seeds]=mcxlab(cfg);
+[flux, detp, vol, seeds]=mcxlabcl(cfg);
 
 newcfg=cfg;
 newcfg.seed=seeds.data;
 newcfg.outputtype='jacobian';
 newcfg.detphotons=detp.data;
-[flux2, detp2, vol2, seeds2]=mcxlab(newcfg);
+[flux2, detp2, vol2, seeds2]=mcxlabcl(newcfg);
 jac=sum(flux2.data,4);
 imagesc(log10(abs(squeeze(jac(:,30,:)))))

pmcxcl/pmcxcl/__init__.py
@@ -37,6 +37,6 @@ except ImportError:  # pragma: no cover
 # from .files import loadmc2, loadmch, load, save
 from .bench import bench
 
-__version__ = "0.0.8"
+__version__ = "0.0.10"
 
 __all__ = ("gpuinfo", "run", "bench")

pmcxcl/setup.py
@@ -119,7 +119,7 @@ class CMakeBuild(build_ext):
 setup(
     name="pmcxcl",
     packages=['pmcxcl'],
-    version="0.0.9",
+    version="0.0.10",
     requires=['numpy'],
     license='GPLv3+',
     author="Matin Raayai Ardakani, Qianqian Fang",

src/Makefile
@@ -152,8 +152,8 @@ mex:        LINKOPT= CXXFLAGS='$$CXXFLAGS -g -DMCX_CONTAINER -DMATLAB_MEX_FILE $
 mex oct:    OUTPUT_DIR=../mcxlabcl
 mex:        OUTPUTFLAG:=-output
 mex:        BINARY=mcxcl
-mex oct:    CUCCOPT+=$(DLLFLAG) -DMCX_CONTAINER
-mex oct:    CCOPT+=$(DLLFLAG) -DMCX_CONTAINER
+mex oct:    CUCCOPT+=$(DLLFLAG) -DMCX_CONTAINER -DMATLAB_MEX_FILE
+mex oct:    CCOPT+=$(DLLFLAG) -DMCX_CONTAINER -DMATLAB_MEX_FILE
 mex:        LINKOPT+=mcxlabcl.cpp -outdir $(OUTPUT_DIR) $(INCLUDEDIRS)
 
 oct:        BINARY=mcxcl.mex

commit 9c86f6b945ce780d2a0c0275fd1a9c987cdacb7d

[bug] fix multi-gpu serialization bug on nvidia GPUs the bug was introduced in https://github.com/fangq/mcxcl/commit/9b4d76f03d86c2c9f3f449e340925649d4514379

src/mcx_core.cl
@@ -1748,10 +1748,14 @@ __kernel void mcx_main_loop(__global const uint* media,
             }
         }
 
+#if defined(MCX_DEBUG_MOVE_ONLY)
+
         if (mediaid == 0 || idx1d == OUTSIDE_VOLUME_MIN || idx1d == OUTSIDE_VOLUME_MAX) {
             printf("ERROR: should never happen! mediaid=%d idx1d=%X gcfg->doreflect=%d n1=%f n2=%f isdet=%d flipdir[3]=%d p=(%f %f %f)[%d %d %d]\n", mediaid, idx1d, GPU_PARAM(gcfg, doreflect), n1, prop.w, isdet, flipdir.w, p.x, p.y, p.z, flipdir.x, flipdir.y, flipdir.z);
             return;
         }
+
+#endif
 
 #endif
     }